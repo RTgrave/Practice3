faa5b5c5b66b9ba0d7d20f69a171e5ab0aed2b73
RTgrave <stalker3067@gmail.com>
2020-09-21 11:23:53 +0300

    Задание 18

---
+++ Practice3.cs
@@ -0,0 +1,25 @@
+using System.Text.RegularExpressions;
+using Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+            Console.ToDouble(Console.ReadLine(x));
+            int n = 0;
+            Console.ToDouble(Console.ReadLine(n));
+            for (int i = 1; i <= n; i++)
+            {
+                x= (x-Match.Pow(x, i))/i;
+            }
+        }
+    }
+}
